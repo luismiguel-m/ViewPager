ce6142aa6b6832ade31fcb6332ef8d4ae69ee881
Luis Miguel <miguel8515@gmail.com>
2019-01-31 23:51:56 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/viewpager/SimpleFragmentPagerAdapter.java
@@ -0,0 +1,28 @@
+package com.example.android.viewpager;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class SimpleFragmentPagerAdapter extends FragmentPagerAdapter {
+
+    public SimpleFragmentPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if (position == 0) {
+            return new MondayFragment();
+        } else if (position == 1) {
+            return new TuesdayFragment();
+        } else {
+            return new WednesdayFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 3;
+    }
+}
